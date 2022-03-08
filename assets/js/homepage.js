b13a0a912365bfdf472d6086d2a7c339bc88ba1c
Kyle Richnafsky <7krich@gmail.com>
2022-03-08 16:30:09 -0500

    form funcitonality added (printing to console

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,4 @@
+// PULL SERVER DATA
 function getUserRepos(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -11,4 +12,26 @@ function getUserRepos(user) {
     });
 };
 
-getUserRepos("7krich");
+// DEFINE VARIABLES
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+// FORM FUNCTION EVENT
+function formSubmitHandler(event) {
+    event.preventDefault();
+    console.log(event);
+
+    //get value from <input> element via nameInputEl DOM variable defined above
+    var username = nameInputEl.value.trim();
+
+    //check that there's a value in the username variable, if so run user repo function & clear out the form
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+// EVENT LISTENER TO RUN EVENT
+userFormEl.addEventListener("submit", formSubmitHandler);
