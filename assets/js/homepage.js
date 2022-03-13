2b38a1948c48297cbac0beeb7ee90e6beaf2156c
Kyle Richnafsky <7krich@gmail.com>
2022-03-13 11:02:32 -0400

    feature repos function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -98,3 +98,18 @@ function displayRepos (repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 };
+
+function getFeaturedRepos(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
